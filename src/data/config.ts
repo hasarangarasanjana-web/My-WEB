8afedfe9cf109cadcbff90d89c5dbb45bf055c36
hasarangarasanjana-web <hasarangarasanjana@gmail.com>
2026-03-25 19:17:57 +0530

    Update config.ts

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,9 +1,9 @@
 const config = {
-  title: "Abhijit Zende | AI Enthusiast",
+  title: "Hasaranga Rasanjana | Developer Engineer Tutor Lecture ",
   description: {
-    long: "Explore the portfolio of Abhijit, a Computer Science Engineer with 1.5 year experience in Devlopment, DevOps & Database management with hands-on experience in deploying & automating workflows, & collaborating with foreign clients. Strong analytical skills, effective communication, and a team-oriented approach to driving process improvements and delivering results.",
+    long: "Explore the portfolio of Hasaranga, aInformation Technology and Mecatronic Engineer with 10 year experience in Devlopment, DevOps & Database management with hands-on experience in deploying & automating workflows, & collaborating with foreign clients. Strong analytical skills, effective communication, and a team-oriented approach to driving process improvements and delivering results.",
     short:
-      "Discover the portfolio of Abhijit, a AI enthusiast solving real life problems in DevOps and Development.",
+      "Discover the portfolio of Hasaranga Rsanjana, a AI enthusiast solving real life problems in DevOps and Development.",
   },
   keywords: [
     "Abhijit",
@@ -30,9 +30,9 @@ const config = {
     "AIOps",
     "Azure",
   ],
-  author: "Abhijit Zende",
-  email: "abhijitzende75@gmail.com",
-  site: "https://abhijitzende.com",
+  author: "HasarangaRasanjana",
+  email: "hasarangarasanjana@gmail.com",
+  site: "https://hasarangarasanjana.com",
 
   get ogImg() {
     return this.site + "/assets/seo/og-image.png";
